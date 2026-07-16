7d9f6ffbec4fd2aacd8a46103f950acdb79bb012
Daniel Olivetti <50579682+quileab@users.noreply.github.com>
2026-07-16 09:25:23 -0300

    Remove mobile-only header row from cart drawer

--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -37,11 +37,7 @@
                             </tr>
                         </thead>
                     </table>
-                    {{-- Vista Mobile: título --}}
-                    <div class="md:hidden flex items-center justify-between px-2 py-1">
-                        <span class="text-sm font-bold text-slate-300">Productos en el carrito</span>
-                        <x-button icon="o-x-mark" class="btn-ghost btn-sm btn-circle border border-warning text-warning hover:bg-warning hover:text-warning-content" wire:click="$toggle('showCart')" />
-                    </div>
+
                 </div>
 
                 {{-- ZONA SCROLLEABLE --}}
